53a7fe5bff1d414fbb51390047d9629c5ffc6540
Krishan Patel <krishan711@gmail.com>
2023-01-09 10:06:39 +0000

    .

--- extension/src/foreground.tsx
+++ extension/src/foreground.tsx
@@ -8,12 +8,6 @@ import { App } from './foreground/app';
 const body = document.querySelector('body');
 const app = document.createElement('div');
 app.id = 'gasbuddy-extension-root';
-// app.style.position = 'fixed';
-// app.style.height = '100%';
-// app.style.width = '100%';
-// app.style.top = '0';
-// app.style.pointerEvents = 'none';
-// app.style.zIndex = '1000';
 body.append(app);
 
 // inject react overlay
